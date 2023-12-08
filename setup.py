d0a121f643c7ee59de088f4e19fd2d0f26d62a72
radroxx <radroxxx@gmail.com>
2023-12-08 08:52:34 +0200

    Fix sintaxis

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Brigadier',
     version='0.0.1',
-    packages=find_packages(include=['brigadier'])
+    packages=find_packages(include=['brigadier']),
     install_requires=[
         'aiohttp',
     ],
