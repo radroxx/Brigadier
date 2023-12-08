500b396a44402a1c6ce0dc00ffc1f0d41518b2eb
radroxx <radroxxx@gmail.com>
2023-12-08 08:51:54 +0200

    Add test req and console script

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ setup(
     name='Brigadier',
     version='0.0.1',
     packages=find_packages(include=['brigadier'])
+    install_requires=[
+        'aiohttp',
+    ],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['brigadier=brigadier:main']
+    }
 )
